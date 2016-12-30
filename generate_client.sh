9faea9a48f5bcd8941af1947e8c57142d19368eb
Andrew Koerner <andrew@k0ner.com>
2016-12-30 05:28:12 +0000

    generate client formatting fix

--- generate_client.sh
+++ generate_client.sh
@@ -175,17 +175,24 @@ else
   fi
   
   echo "Compiling output .ovpn file: $OUTPUT_FILE"
+  
   cat $BASE_CONFIG > $KEY_DIRECTORY/$OUTPUT_FILE
-  printf '\n<ca>\n' >> $KEY_DIRECTORY/$OUTPUT_FILE
-  cat $KEY_DIRECTORY/ca.crt >> $KEY_DIRECTORY/$OUTPUT_FILE
-  printf '</ca>\n<cert>\n' >> $KEY_DIRECTORY/$OUTPUT_FILE
-  cat $KEY_DIRECTORY/$CLIENT_NAME.crt >> $KEY_DIRECTORY/$OUTPUT_FILE
-  printf '</cert>\n<key>\n' >> $KEY_DIRECTORY/$OUTPUT_FILE
-  cat $KEY_DIRECTORY/$CLIENT_NAME.key >> $KEY_DIRECTORY/$OUTPUT_FILE
-  printf '</key>\n<tls-auth>\n' >> $KEY_DIRECTORY/$OUTPUT_FILE
-  cat $OPENVPN_DIRECTORY/ta.key >> $KEY_DIRECTORY/$OUTPUT_FILE
-  printf '</tls-auth>\n' >> $KEY_DIRECTORY/$OUTPUT_FILE
-  printf 'key-direction 1' >> $KEY_DIRECTORY/$OUTPUT_FILE
+  echo '<ca>' >> $KEY_DIRECTORY/$OUTPUT_FILE
+    cat $KEY_DIRECTORY/ca.crt >> $KEY_DIRECTORY/$OUTPUT_FILE
+  echo '</ca>' >> $KEY_DIRECTORY/$OUTPUT_FILE
+  echo '<cert>' >> $KEY_DIRECTORY/$OUTPUT_FILE
+    cat $KEY_DIRECTORY/$CLIENT_NAME.crt >> $KEY_DIRECTORY/$OUTPUT_FILE
+  echo '</cert>' >> $KEY_DIRECTORY/$OUTPUT_FILE
+  echo '<key>' >> $KEY_DIRECTORY/$OUTPUT_FILE
+    cat $KEY_DIRECTORY/$CLIENT_NAME.key >> $KEY_DIRECTORY/$OUTPUT_FILE
+  echo '</key>' >> $KEY_DIRECTORY/$OUTPUT_FILE
+  echo '<tls-auth>' >> $KEY_DIRECTORY/$OUTPUT_FILE
+    cat $OPENVPN_DIRECTORY/ta.key >> $KEY_DIRECTORY/$OUTPUT_FILE
+  echo '</tls-auth>' >> $KEY_DIRECTORY/$OUTPUT_FILE
+  echo 'key-direction 1' >> $KEY_DIRECTORY/$OUTPUT_FILE
+  echo '' >> $KEY_DIRECTORY/$OUTPUT_FILE
+  
+  
   echo "Client ovpn created: $KEY_DIRECTORY/$OUTPUT_FILE"
 
   if [ -z ${OUTPUT_DIRECTORY+x} ]; then 
